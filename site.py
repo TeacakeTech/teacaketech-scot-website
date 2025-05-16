f633d9edae80f45a4443b847b245fefd26647751
James <james@jarofgreen.co.uk>
2025-05-16 22:02:34 +0100

    Start Events and Blogs in DataTig

--- site.py
+++ site.py
@@ -3,6 +3,9 @@ from staticpipes.pipes.copy import PipeCopy
 from staticpipes.pipes.jinja2 import PipeJinja2
 from staticpipes.pipes.exclude_underscore_directories import PipeExcludeUnderscoreDirectories
 from staticpipes.pipes.exclude_dot_directories import PipeExcludeDotDirectories
+from staticpipesdatatig.pipes.load_datatig import PipeLoadDatatig
+from staticpipes.pipes.collection_records_process import PipeCollectionRecordsProcess
+from staticpipes.processes.jinja2 import ProcessJinja2
 
 import os
 
@@ -11,7 +14,26 @@ config = Config(
         PipeExcludeDotDirectories(),
         PipeExcludeUnderscoreDirectories(),
         PipeCopy(extensions=["css", "txt"]),
-        PipeJinja2()
+        PipeLoadDatatig(),
+        PipeJinja2(),
+        PipeCollectionRecordsProcess(
+            collection_name="event",
+            processors=[
+                ProcessJinja2(template="_templates/event.html")
+            ],
+            output_dir="event",
+            context_key_record_id="event_id",
+            context_key_record_data="event"
+        ),
+        PipeCollectionRecordsProcess(
+            collection_name="blog",
+            processors=[
+                ProcessJinja2(template="_templates/blog.html")
+            ],
+            output_dir="blog",
+            context_key_record_id="blog_id",
+            context_key_record_data="blog"
+        ),
     ],
 )
 
---
+++ src/_blog/2025-06-01-example-blog-post.md
@@ -0,0 +1,6 @@
+---
+title:  "Example Blog Post!"
+description: "This is an example."
+---
+
+This is an example blog post
